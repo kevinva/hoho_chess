455c4f5b4ed1a8fded94dc8423420efa63bf6b27
hezhi <hezhi@yy.com>
2022-05-09 17:33:14 +0800

    build agent!

--- model/agent.py
+++ model/agent.py
@@ -1,8 +1,46 @@
+import os
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from config import RESIDUAL_BLOCK_NUM
+
+
+class Agent:
+
+    def __init__(self):
+        self.plane_extractor = PlaneExtractor(IN_PLANES_NUM, OUT_PLANES_NUM, RESIDUAL_BLOCK_NUM).to(DEVICE)
+        self.value_net = ValueNet(OUT_PLANES_NUM, ACTION_DIM).to(DEVICE)
+        self.policy_net = PolicyNet(OUT_PLANES_NUM, ACTION_DIM).to(DEVICE)
+        self.passed = False
+    
+    def predict(self, state):
+        board_features = self.extractor(state)
+        win_score = self.value_net(board_features)
+        prob = self.policy_net(board_features)
+        return prob, win_score
+
+    def save_models(self, state, current_time):
+        for model in ['plane_extractor', 'policy_net', 'value_net']:  # 注意跟属性名对应
+            self._save_checkpoint(getattr(self, model), model, state, current_time)
+
+    def _save_checkpoint(self, model, filename, state, current_time):
+        dir_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'saved_models', current_time)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        
+        filename = os.path.join(dir_path, '{}-{}.pth.tar'.format(filename, state['version']))
+        state['model'] = model.state_dict()
+        torch.save(state, filename)
+
+    def load_models(self, path, models):
+        names = ['plane_extractor', 'policy_net', 'value_net']
+        for i in range(0, len(models)):
+            checkpoint = torch.load(os.path.join(path, models[i]))
+            model = getattr(self, names[i])
+            model.load_state_dict(checkpoint['model'])
+            return checkpoint  # hoho: 确定就在循环内return?
+
 
 class ResidualBlock(nn.Module):
     """
@@ -30,23 +68,24 @@ class ResidualBlock(nn.Module):
         return out
 
 
-class Extractor(nn.Module):
+class PlaneExtractor(nn.Module):
     """
     棋盘state特征提取，
     """
 
-    def __init__(self, inplanes, outplanes):
-        super(Extractor, self).__init__()
+    def __init__(self, inplanes, outplanes, residual_num=RESIDUAL_BLOCK_NUM):
+        super(PlaneExtractor, self).__init__()
         
+        self.residual_num = residual_num
         self.conv1 = nn.Conv2d(inplanes, outplanes, stride=1, kernel_size=3, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(outplanes)
 
-        for block in range(RESIDUAL_BLOCK_NUM):
+        for block in range(self.residual_num):
             setattr(self, 'res{}'.format(block), ResidualBlock(outplanes, outplanes))
     
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
-        for block in range(RESIDUAL_BLOCK_NUM):
+        for block in range(self.residual_num):
             x = getattr(self, 'res{}'.format(block))(x)
 
         return x
@@ -61,7 +100,7 @@ class PolicyNet(nn.Module):
         super(PolicyNet, self).__init__()
 
         self.action_dim = action_dim
-        self.conv = nn.Conv2d(inplanes, 1, kernel_size=1)  # hoho: 原论文不是2个filter么？
+        self.conv = nn.Conv2d(inplanes, 1, kernel_size=1)  # hoho: 原论文不是2个filter么，输出channel应该为2个？
         self.bn = nn.BatchNorm2d(1)
         self.softmax = nn.Softmax(dim=1)
         self.fc = nn.Linear(action_dim - 1, action_dim)   # action_dim为动作数，包括pass这个动作
@@ -79,29 +118,40 @@ class ValueNet(nn.Module):
     输出胜负价值[-1, 1]
     """
 
-    def __init__(self, inplanes, outplanes):
+    def __init__(self, inplanes, mid_dim):
         super(ValueNet, self).__init__()
 
-        self.outplanes = outplanes
-        self.cov = nn.Conv2d(inplanes, 1, kernel_size=1)
+        self.mid_dim = mid_dim
+        self.conv = nn.Conv2d(inplanes, 1, kernel_size=1)
         self.bn = nn.BatchNorm2d(1)
-        self.fc1 = nn.Linear(outplanes - 1, 256)
+        self.fc1 = nn.Linear(mid_dim, 256)
         self.fc2 = nn.Linear(256, 1)
 
     def forward(self, x):
         x = F.relu(self.bn(self.conv(x)))
-        x = x.view(-1, self.outplanes - 1)
+        x = x.view(-1, self.mid_dim)
         x = F.relu(self.fc1(x))
-        winning = F.tanh(self.fc2(x))
-        return winning
+        win_score = torch.tanh(self.fc2(x))
+        return win_score
 
 if __name__ == '__main__':
-    input = torch.ones((1, 1, 5, 10)).float()
-    extractor = Extractor(1, 1)
-    output = extractor(input)
-    print(f'input: {input}')
-    print(f'extractor output: {output}')
+    in_channel = 1
+    out_channel = 1
+    action_dim = 11
 
-    policy_net = PolicyNet(1, 11)
-    output = policy_net(output)
-    print(f'policy_net output: {output}')
+    input = torch.ones((1, in_channel, 5, 10)).float()
+    extractor = PlaneExtractor(in_channel, out_channel)
+    extractor_output = extractor(input)
+    print(f'input: {input}')
+    print(f'extractor output: {extractor_output}')
+
+    policy_net = PolicyNet(out_channel, action_dim)
+    policy_output = policy_net(extractor_output)
+    print(f'policy_net output: {policy_output}')
+
+    value_in_dim = extractor_output.size()[2] * extractor_output.size()[3]
+    value_net = ValueNet(out_channel, value_in_dim)
+    value_output = value_net(extractor_output)
+    print(f'value_new output: {value_output}')
+
+    print(f"{os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'saved_models', '12312412')}")
--- model/config.py
+++ model/config.py
@@ -3,4 +3,10 @@ import torch
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+BOARD_WIDTH = 9
+BOARD_HEIGHT = 10
+HISTORY_NUM = 7
+IN_PLANES_NUM = (HISTORY_NUM + 1) * 2
+OUT_PLANES_NUM = 10
+ACTION_DIM = 0  # hoho: 需要一个函数算出所有落子动作数量
 RESIDUAL_BLOCK_NUM = 256
