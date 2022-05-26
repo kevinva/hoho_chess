3c7d01074d2e912cc96f19a1c842ef8a033ee09b
hezhi <hezhi@yy.com>
2022-05-26 21:32:54 +0800

    gameboard

--- model/utils.py
+++ model/utils.py
@@ -36,45 +36,8 @@ def sample_rotation(state, num=8):
     return np.array(states)
 
 
-# 所有合法走子，一共有2086个走法
-def create_all_moves():
-    moves = []
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    # 士的走法
-    mandarins_labels = ['d7e8', 'e8d7', 'e8f9', 'f9e8', 'd0e1', 'e1d0', 'e1f2', 'f2e1',
-                        'd2e1', 'e1d2', 'e1f0', 'f0e1', 'd9e8', 'e8d9', 'e8f7', 'f7e8']
-    
-    # 象的走法
-    elephants_labels = ['a2c4', 'c4a2', 'c0e2', 'e2c0', 'e2g4', 'g4e2', 'g0i2', 'i2g0',
-                        'a7c9', 'c9a7', 'c5e7', 'e7c5', 'e7g9', 'g9e7', 'g5i7', 'i7g5',
-                        'a2c0', 'c0a2', 'c4e2', 'e2c4', 'e2g0', 'g0e2', 'g4i2', 'i2g4',
-                        'a7c5', 'c5a7', 'c9e7', 'e7c9', 'e7g5', 'g5e7', 'g9i7', 'i7g9']
-
-    cols = len(letters)
-    rows = len(numbers)
-    for c in range(cols):
-        for r in range(rows):
-            # 这里假设起始点为（c, r）,以下计算能走的目标点
-            destinations = [(t, r) for t in range(cols)] + \
-                           [(c, t) for t in range(rows)] + \
-                           [(c + a, r + b) for (a, b) in [(-2, -1), (-1, -2), (-2, 1), (1, -2), (2, -1), (-1, 2), (2, 1), (1, 2)]] # 马走日
-
-        
-            for (c2, r2) in destinations:
-                if (c, r) != (c2, r2) and c2 in range(cols) and r2 in range(rows):
-                    move = letters[c] + numbers[r] + letters[c2] + numbers[r2]
-                    moves.append(move)
-            
-    moves.extend(mandarins_labels)
-    moves.extend(elephants_labels)
-
-    return moves
-
-
-
-if __name__ == '__main__':
-    moves = create_all_moves()
-    print(len(moves))
-    print(moves[:20])
+# if __name__ == '__main__':
+    # moves = create_all_moves()
+    # print(len(moves))
+    # print(moves[:20])
