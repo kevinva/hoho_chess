93286ef19a738ed29d2b982740f74d0bfd67d161
hezhi <hezhi@yy.com>
2022-05-30 17:47:14 +0800

    test

--- demo/asyncio_demo.py
+++ demo/asyncio_demo.py
@@ -15,28 +15,119 @@
 
 import aiohttp
 import asyncio
+import threading
+from datetime import datetime
+import time
 
-def do_requests(session):
-    return session.get('http://rap2api.taobao.org/app/mock/data/2267369')
+# def do_requests(session):
+#     return session.get('http://rap2api.taobao.org/app/mock/data/2267369')
+
+# async def main():
+#     async with aiohttp.ClientSession() as session:
+#         tasks = []
+
+#         for _ in range(0, 10):
+#             tasks.append(do_requests(session))
+
+#         results = await asyncio.gather(*tasks)
+#         for r in results:
+#             print(f'status code: {r.status}')
+
+
+# async def main2():
+#     await asyncio.sleep(1)
+#     print('hello')
+
+
+# async def cancel_me():
+#     print('cancel_me(): sleep')
+#     try:
+#         # Wait for 1 hour
+#         await asyncio.sleep(3600)
+#     except asyncio.CancelledError:
+#         print('cancel_me(): cancel sleep')
+#         raise
+#     finally:
+#         print('cancel_me(): after sleep')
+
+# async def main():
+#     print('main(): running')
+#     # Create a "cancel_me" Task
+#     task = asyncio.create_task(cancel_me())
+
+#     # Wait for 5 second
+#     print('main(): sleep')
+#     await asyncio.sleep(5)
+
+#     print('main(): call cancel')
+#     task.cancel()
+#     try:
+#         await task
+#     except asyncio.CancelledError:
+#         print('main(): cancel_me is cancelled now')
+
+
+# async def cancel_me():
+#     print('cancel_me(): sleep')
+#     try:
+#         await asyncio.sleep(3600)
+#     except asyncio.CancelledError:
+#         print('cancel_me(): cancel sleep')
+#         raise
+#     finally:
+#         print('cancel_me(): after sleep')
+
+
+# async def main():
+#     print('main(): running')
+#     # Create a 'cancel_me' Task
+#     task = asyncio.create_task(cancel_me())
+#     # loop = asyncio.get_event_loop()
+#     # task = loop.create_task(cancel_me())
+
+#     # Wait for 60 second
+#     print('main(): sleep')
+#     await asyncio.sleep(5)
+
+#     print('main(): call cancel')
+#     task.cancel()
+
+#     try:
+#         await task
+#     except asyncio.CancelledError:
+#         print('main(): cancel_me is cancelled now')
+
+def hard_work():
+    print('thread id: ', threading.current_thread())
+    time.sleep(10)
+
+async def do_async_job():
+    # await asyncio.sleep(2)
+    # print(datetime.now().isoformat(), 'thread id: ', threading.current_thread())
+
+    await asyncio.to_thread(hard_work)
+    await asyncio.sleep(1)
+    print('job done!')
+
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        tasks = []
+    # await do_async_job()
+    # await do_async_job()
+    # await do_async_job()
 
-        for _ in range(0, 10):
-            tasks.append(do_requests(session))
+    job1 = do_async_job()
+    job2 = do_async_job()
+    job3 = do_async_job()
+    await asyncio.gather(job1, job2, job3)
 
-        results = await asyncio.gather(*tasks)
-        for r in results:
-            print(f'status code: {r.status}')
+    # try:
+    #     await asyncio.wait_for(do_async_job(), timeout=1)
+    # except asyncio.TimeoutError:
+    #     print('timeout')
 
-
-async def main2():
-    await asyncio.sleep(1)
-    print('hello')
 
 if __name__ == '__main__':
-    # asyncio.run(main())
-
-    # main2()
-    asyncio.run(main2())
+    asyncio.run(main())
+    # loop = asyncio.get_event_loop()
+    # task = loop.create_task(main())
+    # loop.run_until_complete(task)
