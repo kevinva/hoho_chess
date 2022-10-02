05d8d1c6e422748963be0bdd2d1d0f6ff9ecb71a
hezhi <kevinvalentine@outlook.com>
2022-10-02 10:54:42 +0800

    Test

--- Web/model/hoho_utils.py
+++ Web/model/hoho_utils.py
@@ -9,22 +9,22 @@ def config_logger():
     logger = logging.getLogger('hoho_logger')
     logger.setLevel(logging.DEBUG)
 
-    dir_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'log')
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    log_filename = os.path.join(dir_path, f'hoho_log_{int(time.time())}.log')
-    fh = logging.FileHandler(log_filename)
-    fh.setLevel(logging.DEBUG)
+    # dir_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'log')
+    # if not os.path.exists(dir_path):
+    #     os.makedirs(dir_path)
+    # log_filename = os.path.join(dir_path, f'hoho_log_{int(time.time())}.log')
+    # fh = logging.FileHandler(log_filename)
+    # fh.setLevel(logging.DEBUG)
 
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter('[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s')
 
-    fh.setFormatter(formatter)
+    # fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
-    logger.addHandler(fh)
+    # logger.addHandler(fh)
     logger.addHandler(ch)
 
     return logger
