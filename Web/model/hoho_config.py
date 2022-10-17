b4d902407c506e5b5a1606f13fe322e43f311dac
hezhi <kevinvalentine@outlook.com>
2022-10-17 23:54:09 +0800

    Test

--- Web/model/hoho_config.py
+++ Web/model/hoho_config.py
@@ -51,10 +51,10 @@ RESTRICT_ROUND_NUM = 100
 SELF_BATTLE_RESTRICT_ROUND_NUM = 100
 
 # 网络训练完成后自博弈的次数
-SELF_BATTLE_NUM = 400
+SELF_BATTLE_NUM = 200 # 400
 
 # 训练的网络与当前网络的自博弈，超过该阈值即替换当前网络
-SELF_BATTLE_WIN_RATE = 0.5  #0.55
+SELF_BATTLE_WIN_RATE = 0.55
 
 # 控制某些日志是否打印
 DEBUG = True
