5508bd9427e531ea1d8fbe1f8e1ac0b6324a86d1
hezhi <kevinvalentine@outlook.com>
2022-10-24 22:42:53 +0800

    Fix: model update path

--- Web/serv/__main__.py
+++ Web/serv/__main__.py
@@ -40,7 +40,7 @@ def ajax_(request_, response_, route_args_):
 		global match_count, agent_updating, agent_update_accepted, agent_update_path, last_update_finish_time, win_count
 
 		if agent_update_accepted and (agent_update_path is not None):
-			hoho_agent.load_model_from_path(model_path)
+			hoho_agent.load_model_from_path(agent_update_path)
 			LOGGER.info(f'Agent updated! version={hoho_agent.version}')
 			
 			agent_update_accepted = False
